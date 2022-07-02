6c4701af5bac4a045851fc659a14032b73cf90ad
Brad Heffernan <ph3onix83@gmail.com>
2022-07-02 11:24:09 +0930

    qwerty

--- src/views/Options/App.js
+++ src/views/Options/App.js
@@ -1,25 +1,48 @@
 import React from 'react'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Options page</p>
-        <p>
-          Edit <code>src/views/Options/App.js</code> and save.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Options extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      selectedOption: "Students"
+    }
+  }
+  componentDidMount() {
+    // var option = ""
+    // chrome.storage.sync.get('option', function(data) {
+    //   option = data.option;
+    // });
+    // if (option !== "") {
+    //   this.setState({selectedOption: option})
+    // }
+  }
+  handleOptionChange = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+    this.setState({
+      selectedOption: e.target.value
+    });
+    // document.
+    // chrome.storage.sync.set({option: e.target.value});
+  }
+
+  render() {
+    return (
+      <form>
+        <div className="radio">
+          <label>
+            <input type="radio" value="Students" checked={this.state.selectedOption === 'Students'} onChange={this.handleOptionChange} />
+              Students
+          </label>
+          <label>
+            <input type="radio" value="Teachers" checked={this.state.selectedOption === 'Teachers'} onChange={this.handleOptionChange} />
+              Teachers
+          </label>
+        </div>
+      </form>
+    );
+  }
 }
 
-export default App
+export default Options
