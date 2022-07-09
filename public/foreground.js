194486bac638479489b91a8f7371f615b1c1e67f
Brad Heffernan <ph3onix83@gmail.com>
2022-07-10 08:58:14 +0930

    updated

--- public/foreground.js
+++ public/foreground.js
@@ -1,6 +1,14 @@
-
+// ===========================================================
+//                 REPLACEMENT LOGOS
+// ===========================================================
 var url = chrome.extension.getURL('images/logo.png');
+var yturl = chrome.extension.getURL('images/yt.png');
+
+// ===========================================================
+//                 GOOGLE LOGO
+// ===========================================================
 var mini = document.querySelector(".logo img");
+
 if (mini !== null){
     mini.src = url
     mini.classList.add('motionsa')
@@ -14,13 +22,19 @@ if (img !== null){
     img.classList.add('motionsa')
 }
 
-var yturl = chrome.extension.getURL('images/yt.png');
-var ytmini = document.querySelector(".style-scope .ytd-logo");
-console.log(ytmini)
-var newImage = document.createElement('img');
-newImage.setAttribute("src", yturl)
-newImage.setAttribute("alt", yturl)
-newImage.setAttribute("style", "pointer-events: none;display: block;width: 100%;height: 30px;")
-// newItem.innerHTML = 'src="'+yturl+'" alt="youtube logo"';
+// ===========================================================
+//                 YOUTUBE LOGO
+// ===========================================================
 
-ytmini.parentElement.replaceChild(newImage, ytmini);
+var ytmini = document.querySelector(".style-scope .ytd-logo");
+
+if (ytmini !== null){
+    console.log(ytmini)
+    var newImage = document.createElement('img');
+    newImage.setAttribute("src", yturl)
+    newImage.setAttribute("alt", yturl)
+    newImage.setAttribute("style", "pointer-events: none;display: block;width: 100%;height: 30px;")
+    newImage.classList.add('motionsa')
+
+    ytmini.parentElement.replaceChild(newImage, ytmini);
+}
